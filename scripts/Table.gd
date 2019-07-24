c29052f80de19fb2f87fd7ef2f096db352f97c4e
Lelek Game Studio <lelekgamestudio@gmail.com>
2019-07-24 18:25:30 -0300

    Comportamento Cliente Parte 1

--- scripts/Table.gd
+++ scripts/Table.gd
@@ -25,6 +25,8 @@ func _on_area_enter(area):
 	if player.interactions.size() > 0 and player.interactions[0] == self:
 		player.interactions.pop_front()
 		get_node("Sprite").material.set_shader_param("width", 0)
+		if self.customer:
+			self.customer.receive_waiter()
 
 func _on_mouse_enter():
 	set_process(true)
