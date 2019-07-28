d1d559b3f1d3b808b9e18788c42e9f3e735ed8de
Carl Marqs <carl.marqs@email.com>
2019-07-28 17:43:39 -0300

    Trash can added

---
+++ scripts/Trash_Can.gd
@@ -0,0 +1,42 @@
+extends Area2D
+
+onready var player = get_tree().get_root().find_node("Player", true, false)
+
+var mouse_over:bool = false
+
+func _ready():
+	set_process(false)
+	connect("area_entered", self, "_on_area_enter")
+	connect("mouse_entered", self, "_on_mouse_enter")
+	connect("mouse_exited", self, "_on_mouse_exit")
+
+func _process(delta):
+	if self in player.interactions:
+		get_node("Sprite").material.set_shader_param("width", 20)
+	else:
+		set_process(false)
+
+func _input(event):
+	if Input.is_action_just_pressed('click') and mouse_over:
+		if not self in player.interactions:
+			player.interactions.push_back(self)
+
+func _on_area_enter(area):
+	if player.interactions.size() > 0 and player.interactions[0] == self:
+		player.interactions.pop_front()
+		get_node("Sprite").material.set_shader_param("width", 0)
+		
+		if player.hands:
+			player.get_node('Food').queue_free()
+			player.hands = null
+
+
+func _on_mouse_enter():
+	set_process(true)
+	mouse_over = true
+	get_node("Sprite").material.set_shader_param("width", 10)
+
+func _on_mouse_exit():
+	mouse_over = false
+	if not self in player.interactions:
+		get_node("Sprite").material.set_shader_param("width", 0)
